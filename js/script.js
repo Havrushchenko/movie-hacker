7d60835e375fb499dc2e3334b47a046b2ce3cb7a
miklywek <miklywek@gmail.com>
2022-11-23 12:51:54 +0200

    add data to dispkay

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var API_KEY = "https://unogsng.p.rapidapi.com/search?orderby=rating&limit=10";
-var filmsData = [];
+var renderedFilmsList = [];
 var sortByRaiting = document.getElementById("sort-by-rating");
 const options = {
   method: "GET",
@@ -17,75 +17,79 @@ function sortAsc(a, b) {
 
 var grid = document.querySelector(".grid");
 
-fetch("https://unogsng.p.rapidapi.com/search?limit=100", options)
+fetch("https://unogsng.p.rapidapi.com/search?limit=5", options)
   .then(function (response) {
     console.log(response);
     return response.json();
   })
   .then(function (data) {
-    filmsData = data.results;
-    console.log(data);
+    renderedFilmsList = data.results;
+    renderFilms(renderedFilmsList);
+  });
+var renderFilms = function (filmsData) {
+  for (var i = 0; i < filmsData.length; i++) {
+    var movieCard = document.createElement("div");
+    movieCard.classList =
+      "movie-card p-1 block max-w-sm bg-white border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 opacity-100 hover:opacity-90 cursor-default max-w-sm h-auto transition-shadow ease-in-out duration-300 shadow-none hover:shadow-xl";
+    grid.appendChild(movieCard);
 
-    for (var i = 0; i < filmsData.length; i++) {
-      var movieCard = document.createElement("div");
-      movieCard.classList =
-        "movie-card p-1 block max-w-sm bg-white border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 opacity-100 hover:opacity-90 cursor-default max-w-sm h-auto transition-shadow ease-in-out duration-300 shadow-none hover:shadow-xl";
-      grid.appendChild(movieCard);
+    var type = document.createElement("span");
 
-      var type = document.createElement("span");
-
-      if (data.results[i].vtype === "movie") {
-        type.classList.add("movie-type");
-        type.textContent = "Movie";
-        movieCard.appendChild(type);
-      } else if (data.results[i].vtype === "series") {
-        type.classList.add("series-type");
-        type.textContent = "Series";
-        movieCard.appendChild(type);
-      }
-      // TODO: Create div
-      var poster = document.createElement("img");
-      poster.classList = "poster";
-      poster.setAttribute("src", data.results[i].poster);
-      if (!data.results[i].poster || data.results[i].poster === "0") {
-        poster.setAttribute("src", "./assets/img/image-does-not-exist.jpg");
-        movieCard.appendChild(poster);
-      }
+    if (filmsData[i].vtype === "movie") {
+      type.classList.add("movie-type");
+      type.textContent = "Movie";
+      movieCard.appendChild(type);
+    } else if (filmsData[i].vtype === "series") {
+      type.classList.add("series-type");
+      type.textContent = "Series";
+      movieCard.appendChild(type);
+    }
+    // TODO: Create div
+    var poster = document.createElement("img");
+    poster.classList = "poster";
+    poster.setAttribute("src", filmsData[i].poster);
+    if (!filmsData[i].poster || filmsData[i].poster === "0") {
+      poster.setAttribute("src", "./assets/img/image-does-not-exist.jpg");
       movieCard.appendChild(poster);
+    }
+    movieCard.appendChild(poster);
 
-      var movieTitle = document.createElement("h2");
-      movieTitle.classList =
-        "title mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center";
-      movieTitle.textContent = data.results[i].title;
-      movieCard.appendChild(movieTitle);
+    var movieTitle = document.createElement("h2");
+    movieTitle.classList =
+      "title mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center";
+    movieTitle.textContent = filmsData[i].title;
+    movieCard.appendChild(movieTitle);
 
-      var movieYear = document.createElement("p");
-      movieYear.classList =
-        "font-normal text-sm p-1 text-gray-700 dark:text-gray-400";
-      movieYear.textContent = "Year:" + " " + data.results[i].year;
-      movieCard.appendChild(movieYear);
+    var movieYear = document.createElement("p");
+    movieYear.classList =
+      "font-normal text-sm p-1 text-gray-700 dark:text-gray-400";
+    movieYear.textContent = "Year:" + " " + filmsData[i].year;
+    movieCard.appendChild(movieYear);
 
-      var rating = document.createElement("p");
+    var rating = document.createElement("p");
+    rating.classList =
+      "font-normal text-sm p-1 text-gray-700 dark:text-gray-400";
+    rating.textContent = "IMDb:" + " " + filmsData[i].imdbrating;
+    if (!filmsData[i].imdbrating) {
       rating.classList =
         "font-normal text-sm p-1 text-gray-700 dark:text-gray-400";
-      rating.textContent = "IMDb:" + " " + data.results[i].imdbrating;
-      if (!data.results[i].imdbrating) {
-        rating.classList =
-          "font-normal text-sm p-1 text-gray-700 dark:text-gray-400";
-        rating.textContent = "IMDb: N/A";
-      }
-      movieCard.appendChild(rating);
+      rating.textContent = "IMDb: N/A";
     }
-  });
+    movieCard.appendChild(rating);
+  }
+};
 
 sortByRaiting.addEventListener("change", function (e) {
   console.log(e);
 
   var value = e.target.value;
   if (value === "asc") {
-    filmsData.sort(sortAsc);
+    renderedFilmsList.sort(sortAsc);
+    console.log(renderFilms(renderedFilmsList));
   } else {
-    filmsData.sort(sortDescend);
+    renderedFilmsList.sort(sortDescend);
   }
-  console.log(filmsData);
+  console.log(renderedFilmsList);
+  renderFilms(renderedFilmsList);
+  console.log(renderFilms(renderedFilmsList));
 });
