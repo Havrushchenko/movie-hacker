99a645b7e526fcb03ef80bee9c109b9b744fd49f
miklywek <miklywek@gmail.com>
2022-11-25 21:07:32 +0200

    fe

--- js/script.js
+++ js/script.js
@@ -96,3 +96,24 @@ sortByRaiting.addEventListener("change", function (e) {
   }
   renderFilms(renderedFilmsList);
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+  const getSearchBar = 
+      document.querySelector('#searchBar');
+  const getAllMovies = 
+      document.querySelectorAll('.movieWrapper');
+
+  getSearchBar.addEventListener('keyup', e => {
+      getAllMovies.forEach(movie => {
+          if (movie.innerText.toLowerCase().includes(
+              e.target.value.toLowerCase())) {
+              movie.style.display = 'block';
+              return movie;
+          }
+          else {
+              movie.style.display = 'none';
+          }
+      });
+  });
+});
